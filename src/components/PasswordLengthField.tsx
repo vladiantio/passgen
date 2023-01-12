148de1b27920c0f5c22ce9018b4bb685c59d8b4c
Vladimir Figueroa <vladimir.antonio.f@gmail.com>
2023-01-12 15:30:54 -0300

    Custom ranger input

--- src/components/PasswordLengthField.tsx
+++ src/components/PasswordLengthField.tsx
@@ -1,30 +1,55 @@
 import { ControllerRenderProps } from 'react-hook-form';
+import { Range } from 'react-range';
 
 type PasswordLengthFieldProps = {
   field: ControllerRenderProps<any, any>;
 };
 
 const PasswordLengthField = ({ field }: PasswordLengthFieldProps) => {
-  const min = 4, max = 64;
+  const min = 4,
+    max = 64;
+  const widthPercent = ((field.value - min) * 100) / (max - min);
   return (
     <div className="flex-none flex flex-col space-y-2">
       <label htmlFor="inputPasswordLength">Longitud</label>
       <div className="flex items-center space-x-4">
         <input
           type="number"
-          className="form-input dark:bg-neutral-800 transition border-neutral-200 dark:border-neutral-700 rounded-xl shadow"
+          className="form-input w-20 dark:bg-neutral-800 transition border-neutral-200 dark:border-neutral-700 rounded-xl shadow"
           id="inputPasswordLength"
           min={min}
           max={max}
           {...field}
         />
-        <input
-          type="range"
-          className="flex-1"
-          id="rangePasswordLength"
+        <Range
+          values={[field.value]}
+          step={1}
           min={min}
           max={max}
-          {...field}
+          onChange={(values) => {
+            if (field.value != values[0])
+              field.onChange({ target: { value: values[0] } });
+          }}
+          renderTrack={({ props, children }) => (
+            <div
+              {...props}
+              className="flex-1 flex w-full bg-neutral-200 dark:bg-neutral-800 rounded-full h-2.5"
+            >
+              <div
+                className="bg-blue-600 h-2.5 rounded-full"
+                style={{
+                  width: `${widthPercent}%`,
+                }}
+              ></div>
+              {children}
+            </div>
+          )}
+          renderThumb={({ props }) => (
+            <div
+              {...props}
+              className="w-5 h-5 rounded-full bg-blue-600 shadow-sm transition-shadow focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-40"
+            />
+          )}
         />
       </div>
     </div>
